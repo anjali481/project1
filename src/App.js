ffe3cd67039d66e30050fcb7ed0f6599c94711b2
anjali481 <55936581+anjali481@users.noreply.github.com>
2020-08-12 21:59:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {    
+	BrowserRouter,
+	Switch,
+	Route,
+	Link,
+	useParams
+  } from "react-router-dom";
+import  Page from './components/Page'
+
+class App extends React.Component{
+  render() {
+	return (
+	  <BrowserRouter>
+	  <div className="App">
+		  <Route exact path="/:page_id" component={Page}/>
+
+		  </div>
+		  </BrowserRouter>
+	);
+  }
+}
+
+export default App;
